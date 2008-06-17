083571399ff5f860dde77fb6a0e3779dba0dda0c
Arne Halvorsen <arne.halvorsen@gmail.com>
2008-06-17 10:14:02 +0000

    Made Modhex alphabet String a public constant: Other code may want to know, this is the proper location for it. Renamed to ALPHABET: Modhex.ALPHABET reads well in code.
    Made decode accept both lower and upper case.
    
    Added java doc.
    
    Private constructor.
    
    Formated to soft tab of two white spaces.
    
    Removed commented away line and not used variable b.

--- src/com/yubico/base/Modhex.java
+++ src/com/yubico/base/Modhex.java
@@ -3,49 +3,87 @@ package com.yubico.base;
 
 import java.io.ByteArrayOutputStream;
 
+/**
+ * <p>
+ *   Utility methods to 
+ *   {@link #encode(byte[]) encode} a byte array to Modhex 
+ *   {@code String} and to
+ *   {@link #decode(String) decode} a Modhex
+ *   {@link String} to a the byte array it represents.   
+ * </p>
+ * <p>
+ *   Modehex encoding uses the 
+ *   {@link #ALPHABET alphabet} {@code cbdefghijklnrtuv} which has the property
+ *   of being at the same position on all keyboards.
+ * </p>
+ * @author Simon
+ */
 public class Modhex
 {
-    private static String transString = "cbdefghijklnrtuv";
-    private static char trans[] = transString.toCharArray();
+  private Modhex(){} // Utility pattern dictates private constructor.
+  
+  /**
+  * <p>
+  *   The Modhex alphabet: the letters used to decode bytes in.
+  * </p>
+  */
+  public final static String ALPHABET = "cbdefghijklnrtuv";
+    
+  private static char trans[] = ALPHABET.toCharArray();
 
-    public static String encode(byte[] data)
-    {
-	//byte[] data = s.getBytes();
-        StringBuffer result = new StringBuffer();
+  /**
+   * <p>
+   *   Encodes.
+   * </p>
+   * @param data Data to encode.
+   * @return Modhex.
+   */
+  public static String encode(byte[] data)
+  {
+	  StringBuffer result = new StringBuffer();
 
-        for (int i = 0; i < data.length; i++) {
+    for (int i = 0; i < data.length; i++) {
 	    result.append(trans[(data[i] >> 4) & 0xf]);
 	    result.append(trans[data[i] & 0xf]);
-	}
+	  }
 
-        return result.toString();
-    }
+    return result.toString();
+  }
 
-    public static byte[] decode(String s)
-    {
-	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int len = s.length();
- 	int b;
-	boolean toggle = false;
-	int keep = 0;
+  /**
+   * <p>
+   *   Decodes.
+   * </p>
+   * @param s Modhex encoded
+   *          {@link String}. Decoding ignores case of {@code s}.
+   * @return Bytes {@code s} represents.
+   * @throws IllegalArgumentException If {@code s} not valid Modhex.
+   */
+  public static byte[] decode(String s)
+  {
+	  ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    int len = s.length();
+ 	  
+	  boolean toggle = false;
+	  int keep = 0;
 
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
- 	    int n = transString.indexOf(ch);
+    for (int i = 0; i < len; i++) {
+      char ch = s.charAt(i);
+ 	    int n = ALPHABET.indexOf(Character.toLowerCase(ch));
 	    if (n == -1) {
-		throw new 
-		    IllegalArgumentException(s+" is not properly encoded");
+		    throw new 
+		      IllegalArgumentException(s+" is not properly encoded");
 	    }
 
  	    toggle = !toggle;
 
  	    if (toggle) {
-		keep = n;
+		    keep = n;
 	    } else {
-		baos.write((keep << 4) | n);
+		    baos.write((keep << 4) | n);
  	    }
- 	}
-	return baos.toByteArray();
-     }
+ 	  }
+	  return baos.toByteArray();
+  }
+  
 }
-
